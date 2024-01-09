97a3bc8e2e69eabdc1d2525d6d3ddb97e1ca33d3
samojeyinka <samuelojeyinka@gmail.com>
2024-01-09 01:24:14 -1200

    done with theme switch and desktop nav

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
 import { ThemeProvider, useTheme } from './contexts/ThemeContext';
+import Articles from './pages/Articles'
 
 function App() {
   const { theme, toggleTheme } = useTheme();
   return (
-    <main className={`App ${theme}`}>
-             <header>
-        <h1>React Dark Mode</h1>
-        <button onClick={toggleTheme}>Toggle Theme</button>
-      </header>
-      <main>
-        <p>This is a sample React app with light and dark mode.</p>
-      </main>
+  
+    <div className={` app ${theme}`}>
+        <Header/>   
+    </div>
 
-    </main>
+
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,38 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { BiSun, BiMoon } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
+import { useTheme } from '../contexts/ThemeContext';
 
 
 const Header = () => {
-
-
-   
+    const { theme, toggleTheme} = useTheme();
 
     return (
-        <header>
+        <header className={`${theme}`}>
         <nav className="navbar">
             <ul className="menu-links">
-                <li>Home</li>
-                <li>Projects</li>
-                <li>Contact</li>
-                <li>Articles</li>
+                <li>
+                    <Link to={'/about'}>About</Link>
+                </li>
+                <li>
+                    <Link to={'/articles'}>Articles</Link>
+                </li>
+                <li>
+                    <Link to={'/projects'}>Projects</Link>
+                </li>
+                <li>
+                    <Link to={'/apis'}>APIs</Link>
+                </li>
+                <li>
+                    <Link to={'/contact'}>Contact</Link>
+                </li>
             </ul>
+            <div className="theme-switch" onClick={toggleTheme}>
+                {theme === 'dark-theme' ? (
+                <BiSun size={25} className='icon'/>)
+                : (<BiMoon size={25} className='icon'/>)
+}
+            </div>
         </nav>
         </header>
     );
